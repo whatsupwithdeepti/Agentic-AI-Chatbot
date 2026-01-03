3f1e8cc27e35e60f523f7e8a48c6312bb0016e0e
deepti jethwani <deeptijethwaniatwork@gmail.com>
2026-01-03 11:44:47 +0530

    Modified Graph, nodes and state files

--- src/LanggraphAgenticAI/Graph/graph_builder.py
+++ src/LanggraphAgenticAI/Graph/graph_builder.py
@@ -1,5 +1,6 @@
 from langgraph.graph import StateGraph, START, END
 from src.LanggraphAgenticAI.state.state import State
+from src.LanggraphAgenticAI.nodes.basic_chatbot_node import BasicChatbotNode
 
 class GraphBuilder:
     def __init__(self, model):
@@ -14,6 +15,8 @@ class GraphBuilder:
         entry and exit point of the graph.
         """ 
 
-        self.graph_builder.add_node('chatbot', "")
+        self.basic_chatbot_node=BasicChatbotNode(self.llm)
+
+        self.graph_builder.add_node('chatbot', self.basic_chatbot_node.process)
         self.graph_builder.add_edge(START, "chatbot")
         self.graph_builder.add_edge("chatbot", END)
