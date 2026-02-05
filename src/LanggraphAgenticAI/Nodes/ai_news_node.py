4e0ddb0ad90ba398b62ab03f52116eba023a085c
deepti jethwani <deeptijethwaniatwork@gmail.com>
2026-02-05 12:45:19 +0530

    feat: created ai news node functionalities

---
+++ src/LanggraphAgenticAI/Nodes/ai_news_node.py
@@ -0,0 +1,76 @@
+from tavily import TavilyClient
+from langchain_core.prompts import ChatPromptTemplate
+
+class AINewsNode:
+    def __init__(self,llm):
+        """
+        Initialize the AI News Node with the given language model.
+        """
+        self.tavily = TavilyClient()
+        self.llm = llm
+        # this is used to capture various steps in this file so that later can be use for steps shown
+        self.state = {}
+    
+    def fetch_news(self, state: dict) -> dict:
+        """
+        Fetch the latest AI news based on the selected time frame and update the state with the news data.
+
+        Args:
+            state (dict): The state dictionary containing 'frequency'.
+            
+        Returns:
+            dict: Updated state with 'news_data' key containing fetched news data.
+        """
+
+        frequency = state['messages'][0].content.lower()
+        self.state['frequency'] = frequency
+        time_range_map = {"daily": 'd', 'weekly': 'w', 'monthly': 'm', 'year': 'y'}
+        days_map = {'daily': 1, 'weekly': 7, 'monthly': 30, 'yearly': 365}
+
+        response = self.tavily.search(
+            query = "Top Artificial Intelligence (AI) technology news India and globally",
+            topic = "news",
+            time_range=time_range_map[frequency],
+            include_answer = 'advanced',
+            max_results=15,
+            days=days_map[frequency],
+            #include_domains=['techcrunch.com','venturebeat.com/ai','thenextweb.com','wired.com','theverge.com']
+        )
+
+        state['news_data'] = response.get('results', [])
+        self.state['news_data'] = state['news_data']
+        return state
+    
+    def summarize_news(self, state: dict) -> dict:
+        """
+        Summarize the fetched news data using the language model and update the state with the summaries.
+
+        Args:
+            state (dict): The state dictionary containing 'news_data'.
+            
+        Returns:
+            dict: Updated state with 'news_summaries' key containing summarized news data.
+        """
+        news_data = self.state['news_data']
+
+        prompt_template = ChatPromptTemplate.from_messages([
+            ("system", """Summarize AI news articles into markdown format. for each item include:
+             - Date in `YYYY-MM-DD` format in IST timezone
+             - Concise sentences summary from latest news
+             - Sort news by date wise (latest first)
+             - Source URL as clickable link
+             Use format:
+             ### [Date]
+             - [Summary](URL)"""),
+             ("user", "Articles: \n{articles}")
+        ])
+        
+        articles_str = "\n\n".join([
+            f"Content: {item.get('content', '')}\nURL: {item.get('url', '')}\nDate: {item.get('published_at', '')}"
+            for item in news_data
+        ])
+
+        response = self.llm.invoke(prompt_template.format(articles=articles_str))
+        state['summary'] = response.content
+        self.state['summary'] = state['summary']
+        return self.state
