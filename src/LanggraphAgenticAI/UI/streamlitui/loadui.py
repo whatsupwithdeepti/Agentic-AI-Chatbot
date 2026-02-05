690a36e49be4f64941d29fe4f7045394796ce15d
deepti jethwani <deeptijethwaniatwork@gmail.com>
2026-02-05 11:26:07 +0530

    feat: add tavily as an environ

--- src/LanggraphAgenticAI/UI/streamlitui/loadui.py
+++ src/LanggraphAgenticAI/UI/streamlitui/loadui.py
@@ -33,7 +33,7 @@ class LoadStreamlitUI:
             self.user_controls["selected_usecase"]=st.selectbox("Select Usecases", usecase_options)
 
             if self.user_controls["selected_usecase"] == "Chatbot with Tool":
-                self.user_controls["TAVILY_API_KEY"]=st.session_state["TAVILY_API_KEY"]=st.text_input("TAVILY_API_KEY",type="password")
+                os.environ["TAVILY_API_KEY"] = st.session_state["TAVILY_API_KEY"]=st.text_input("TAVILY_API_KEY",type="password")
 
                 #Validate API Key
                 if not self.user_controls["TAVILY_API_KEY"]:
