d0468206a83cd89214c1afad0347905aedfd5934
deepti jethwani <deeptijethwaniatwork@gmail.com>
2026-02-05 11:45:42 +0530

    feat: add AI news option

--- src/LanggraphAgenticAI/UI/streamlitui/loadui.py
+++ src/LanggraphAgenticAI/UI/streamlitui/loadui.py
@@ -32,11 +32,21 @@ class LoadStreamlitUI:
             ## Usecase Selection
             self.user_controls["selected_usecase"]=st.selectbox("Select Usecases", usecase_options)
 
-            if self.user_controls["selected_usecase"] == "Chatbot with Tool":
+            if self.user_controls["selected_usecase"] == "Chatbot with Tool" or self.user_controls["selected_usecase"] == "AI News":
                 os.environ["TAVILY_API_KEY"] = st.session_state["TAVILY_API_KEY"]=st.text_input("TAVILY_API_KEY",type="password")
 
                 #Validate API Key
                 if not self.user_controls["TAVILY_API_KEY"]:
                     st.warning("Please enter your TAVILY_API_KEY key to proceed. Don't have? refer : https://app.tavily.com/home")
+
+            if self.user_controls["selected_usecase"] == "AI News":
+                st.subheader("📰 AI News Explorer")
+
+                with st.sidebar:
+                    time_frame = st.selectbox("🗓️ Select Time Frame", ["Daily", "Weekly", "Monthly"], index=0)
+
+                if st.button("🔍 Fetch Latest AI News", use_container_width=True):
+                    st.session_state.IsFetchButtonClicked = True
+                    st.session_state.timeframe = time_frame
 
         return self.user_controls
